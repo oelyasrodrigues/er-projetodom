da8c365ab0543201682d0e0b8185612c7d30e388
Elyas Rodrigues <elyasrz11@gmail.com>
2023-04-09 16:26:10 -0300

    criação do projeto e da seção HERO

---
+++ src/components/hero/index.tsx
@@ -0,0 +1,24 @@
+import styles from "./styles.module.scss";
+
+export default function Hero() {
+  return (
+    <>
+      <section className={styles.hero}>
+        <div className="container">
+          <div className={styles.content}>
+            <h1>APRENDA A COMO SE TORNAR UM VERDADEIRO <span>CHEFE DE FAMÍLIA</span></h1>
+            <p>
+              Descubra os segredos das ruas, aprenda a vencer seus limites e
+              conquiste a vida que você sempre sonhou. Se você busca a melhor
+              versão de si mesmo e é capaz de enfrentar qualquer desafio, esse
+              curso é para você.
+            </p>
+            <a className="btn" href="#">
+              QUERO ENTRAR PARA A FAMÍLIA
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
